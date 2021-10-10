c617c9bc76d852c338a4d3f0d99c843ba3a47fd2
uiandwe <uiandwe@gmail.com>
2021-10-10 23:11:02 +0900

    init data 추가

---
+++ README.md
@@ -0,0 +1,34 @@
+# Dillinger
+## _The Last Markdown Editor, Ever_
+
+## init load data
+```sh
+$ python manage_local.py loaddata ./blog/init_data/blog.json
+```
+
+## run test
+```sh
+$ pytest
+```
+
+
+## 실행
+```sh
+$ python manage_local.py runserver
+```
+
+[http://127.0.0.1:8000/](http://127.0.0.1:8000/)
+
+
+____
+
+## API 리스트
+| Plugin | README |
+| ------ | ------ |
+| Dropbox | [plugins/dropbox/README.md][PlDb] |
+| GitHub | [plugins/github/README.md][PlGh] |
+| Google Drive | [plugins/googledrive/README.md][PlGd] |
+| OneDrive | [plugins/onedrive/README.md][PlOd] |
+| Medium | [plugins/medium/README.md][PlMe] |
+| Google Analytics | [plugins/googleanalytics/README.md][PlGa] |
+
--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
 class Post(models.Model):
     id = models.IntegerField(primary_key=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return "{}, {}, {}".format(self.email, self.content, self.created)
