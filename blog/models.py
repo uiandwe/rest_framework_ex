119d09daabeba140e586b541a40c26d0fb9ef640
uiandwe <uiandwe@gmail.com>
2021-10-10 15:17:21 +0900

    settings 나누기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    id = models.IntegerField(primary_key=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
